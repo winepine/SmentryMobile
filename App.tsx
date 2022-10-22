ac8d890da3404e39a397e9b6d86ad9b5bab13b2e
Basit Saeed <69200255+winepine@users.noreply.github.com>
2022-10-22 17:13:52 +0500

    Setting Up Firestore SDK & Todo App for Testing

--- App.tsx
+++ App.tsx
@@ -1,23 +1,29 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
-
-interface Props {
-  id: Number;
-}
-export default function App(id: Props) {
+import { ScrollView, StyleSheet, Text, View } from "react-native";
+import firestore from "@react-native-firebase/firestore";
+import { Appbar, Button, TextInput } from "react-native-paper";
+import { useState } from "react";
+export default function App() {
+  const ref = firestore().collection("todos");
+  const [todo, setTodo] = useState<string>("");
+  const addTodo = async () => {
+    const response = await ref.add({
+      title: todo,
+      completed: false,
+    });
+    setTodo("");
+    console.log({ response });
+  };
   return (
-    <View style={styles.container}>
-      <Text>Open up App.ts to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <Appbar>
+        <Appbar.Content title={"TODOs List"} />
+      </Appbar>
+      <ScrollView style={{ flex: 1 }}>
+        <Text>List of TODOs!</Text>
+      </ScrollView>
+      <TextInput label={"New Todo"} onChangeText={setTodo} value={todo} />
+      <Button onPress={() => addTodo()}>Add TODO</Button>
+    </>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
