1a560ab6a9efaa200875b4a83cd12c01c5cc7945
Basit Saeed <69200255+winepine@users.noreply.github.com>
2023-02-19 13:59:55 +0500

    Improvement:SplashScreen Removed

--- App.tsx
+++ App.tsx
@@ -1,16 +1,4 @@
 import CustomNavigationStack from "./components/NavigationStack";
-import * as SplashScreen from "expo-splash-screen";
-import { useCallback, useEffect, useState } from "react";
-SplashScreen.preventAutoHideAsync();
 export default function App() {
-  const [appIsReady, setAppIsReady] = useState(false);
-  useEffect(() => {
-    setAppIsReady(true);
-  }, []);
-  const onLayoutRootView = useCallback(async () => {
-    if (appIsReady) {
-      await SplashScreen.hideAsync();
-    }
-  }, [appIsReady]);
   return <CustomNavigationStack />;
 }
