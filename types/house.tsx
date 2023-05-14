1e534a815c6b19ba2e389f02a18ac25ff32db909
Basit Saeed <69200255+winepine@users.noreply.github.com>
2023-05-14 20:51:01 +0500

    contexts

---
+++ types/house.tsx
@@ -0,0 +1,13 @@
+export interface House {
+  block: string;
+  createdAt: Date;
+  house_no: string;
+  owner_contact: string;
+  owner_name: string;
+  password: string;
+  residents: Array<Resident>;
+  status: string;
+  vehicles: Array<Vehicle>;
+}
+export type Resident = { name: string; cnic: string };
+export type Vehicle = { name: string; numberplate: string };
